22918a42bf79bd3c3135b0d6ce3418c217b5e08a
jdshaeffer <shaefferjd@gmail.com>
2020-05-16 15:44:43 -0600

    content spacing, update index photo

--- components/Content.js
+++ components/Content.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const ContentWrapper = styled.div`
 	color: #FAE0C5;
   font-size: ${props => props.fontSize || '18px'};
-  margin-top: 150px;
+  margin-top: 100px;
 `;
 
 // width should be 90% at smaller screens underneath below width
