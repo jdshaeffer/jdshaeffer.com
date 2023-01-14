72cda835ea63928a5ed98996e1d6ddb636433f44
J.D. Shaeffer <shaefferjd@gmail.com>
2023-01-14 17:16:58 -0500

    replace with svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<main>
+  <div class='app'>
+    <h2>
+      JD Shaeffer
+    </h2>
+    <small>
+      Software Engineer at Butterfly Network, Inc.
+    </small>
+    <small>
+      Find me on <a href='https://github.com/jdshaeffer/'>GitHub</a> or
+      <a href='https://www.linkedin.com/in/jd-s-0b5751132/'>LinkedIn</a>.
+    </small>
+    <h4>
+      Some projects:
+    </h4>
+    <ul style='margin-top: -20px'>
+      <small>
+        <li>
+          <a href='https://nycmud.com/'>NYCMUD</a>: an online multiplayer text
+          game set in UWS NYC.
+        </li>
+      </small>
+    </ul>
+  </div>
+</main>
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		font-family: monospace;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+
+	.app {
+		text-align: left;
+		background-color: #282c34;
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		font-size: 27px;
+		color: white;
+    padding-left: 50px;
+    padding-top: 100px;
+	}
+</style>
