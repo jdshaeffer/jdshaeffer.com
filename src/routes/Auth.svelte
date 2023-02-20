914ce18b36b1d1bf57bda71cac7538f0b057b854
J.D. Shaeffer <shaefferjd@gmail.com>
2023-02-19 19:58:03 -0500

    new deploy

--- src/routes/Auth.svelte
+++ src/routes/Auth.svelte
@@ -9,7 +9,7 @@
 			loading = true;
 			const { error } = await supabase.auth.signInWithOtp({ email });
 			if (error) throw error;
-			alert('Check your email for the login link!');
+			alert('Check your email for the login link.');
 		} catch (error) {
 			if (error instanceof Error) {
 				alert(error.message);
