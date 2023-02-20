d6a040a1d8cd5fa9cc0febb575362bc733b865cc
J.D. Shaeffer <shaefferjd@gmail.com>
2023-02-20 09:18:38 -0500

    fix

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
-import { env } from '$env/dynamic/public';
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 
-console.log('anon', env.PUBLIC_SUPABASE_ANON_KEY);
-console.log('url', env.PUBLIC_SUPABASE_URL);
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL!, env.PUBLIC_SUPABASE_ANON_KEY!);
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY!);
