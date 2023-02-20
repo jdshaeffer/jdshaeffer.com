6a374b9fc81f2059a0200fd93ad4ddf1b2f0c01d
J.D. Shaeffer <shaefferjd@gmail.com>
2023-02-19 20:37:16 -0500

    further linking w vercel

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
-import { env } from '$env/dynamic/public';
 
 export const supabase = createClient(
-	// @ts-ignore
-	env.NEXT_PUBLIC_SUPABASE_URL!,
-	// @ts-ignore
-	env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+	process.env.NEXT_PUBLIC_SUPABASE_URL!,
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
