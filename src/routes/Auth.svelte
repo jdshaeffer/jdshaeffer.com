5481208aad90445b09fc4386367d8b02c45c7e0d
J.D. Shaeffer <shaefferjd@gmail.com>
2023-02-20 18:32:22 -0500

    fix

--- src/routes/Auth.svelte
+++ src/routes/Auth.svelte
@@ -6,8 +6,8 @@
 
 	const getURL = () => {
 		let url =
-			process?.env?.NEXT_PUBLIC_SITE_URL ??
-			process?.env?.NEXT_PUBLIC_VERCEL_URL ??
+			process.env.NEXT_PUBLIC_SITE_URL ??
+			process.env.NEXT_PUBLIC_VERCEL_URL ??
 			'http://localhost:5173/';
 		url = url.includes('http') ? url : `https://${url}`;
 		url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
